8a9adee135007cfdd14479380995e47650c2d620
SneakyBuzZ <kaushikx304@gmail.com>
2024-03-30 01:12:33 +0530

    Optimized

--- src/components/StatsComp.tsx
+++ src/components/StatsComp.tsx
@@ -11,21 +11,21 @@ function StatsComp() {
           professional growth and accomplishments.
         </h1>
         <div className="w-full h-1/2 flex flex-row gap-2">
-          <div className="lg:w-1/3 w-1/2 h-36 md:h-56 hover-effect-white center-in-flex-col">
+          <div className="lg:w-1/3 w-1/2 h-44 md:h-56 hover-effect-white center-in-flex-col">
             <h1 className="text-[#002A6A] font-regular">Languages</h1>
-            <div className="size-36">
+            <div className="size-24 md:size-36">
               <DoughnutComp />
             </div>
           </div>
-          <div className="lg:w-1/3 hidden h-36 md:h-56 lg:flex lg:flex-col justify-center items-center gap-3 p-4 bg-[#ffffffc1] border border-[#ffffff09] hover:border-[#3535353a] rounded-md">
+          <div className="lg:w-1/3 hidden  md:h-56 lg:flex lg:flex-col justify-center items-center gap-3 p-4 bg-[#ffffffc1] border border-[#ffffff09] hover:border-[#3535353a] rounded-md">
             <h1 className="text-[#002A6A] font-regular">Frameworks</h1>
-            <div className="size-36">
+            <div className="size-24 md:size-36">
               <DoughnutComp2 />
             </div>
           </div>
-          <div className="lg:w-1/3 w-1/2 h-36 md:h-56 hover-effect-white center-in-flex-col">
+          <div className="lg:w-1/3 w-1/2 h-44 md:h-56 hover-effect-white center-in-flex-col">
             <h1 className="text-[#002A6A] font-regular">Contributions</h1>
-            <div className="size-48">
+            <div className="size-24 w-36 md:size-36">
               <BarComp />
             </div>
           </div>
@@ -33,11 +33,11 @@ function StatsComp() {
         <div className="h-1/2 center-in-flex-row w-full gap-2">
           <div className="md:w-2/5 h-36 md:h-56 hidden md:flex md:flex-col justify-center items-center gap-3 p-4 bg-[#ffffffc1] border border-[#ffffff09] hover:border-[#3535353a] rounded-md ">
             <h1 className="text-[#002A6A] font-regular ">Databases</h1>
-            <div className="size-48">
+            <div className="size-24 md:size-36">
               <BarComp2 />
             </div>
           </div>
-          <div className="md:w-3/5 h-36 md:h-56 hover-effect-white center-in-flex-col">
+          <div className="md:w-3/5 md:h-56 hover-effect-white center-in-flex-col">
             <h1 className="text-[#002A6A] font-regular">
               Contributions vs Hours
             </h1>
